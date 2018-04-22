ff3d00c0dc5a981d21864828246e40c3f1611737
Adam Green <adam.green@adgefficiency.com>
2018-04-22 22:06:08 +0100

    refactor and documenting

--- clean_data.py
+++ clean_data.py
@@ -1,48 +1,26 @@
 """
-Cleans raw ELEXON data
-"""
-import pickle
+Cleans raw ELEXON data that was scraped using scrape_data.py
 
+Each report requires a slightly different approach for cleaning
+"""
 import pandas as pd
 
-price = pd.read_csv('data/B1770.csv', parse_dates=True)
-
-price = price.pivot_table(values='imbalancePriceAmountGBP',
-                          index='time_stamp',
-                          columns='priceCategory')
-
-price.index = pd.to_datetime(price.index)
-
-vol = pd.read_csv('data/B1780.csv', index_col=0, parse_dates=True)
-vol = vol.set_index('time_stamp', drop=True).sort_index()
-vol.index = pd.to_datetime(vol.index)
 
 def print_duplicates(df):
     dupes = df[df.index.duplicated()]
     num = dupes.shape[0]
-
     print('{} duplicates'.format(num))
     if num != 0:
           print('duplicates are:')
           print(dupes.head())
+    return num
+
 
 def remove_duplicates(df):
     print('removing duplicates for {}'.format(df.columns))
     print_duplicates(df)
     return df[~df.index.duplicated(keep='first')]
 
-vol = remove_duplicates(vol)
-price = remove_duplicates(price)
-
-merged = pd.concat([price, vol], axis=1)
-
-idx = pd.DatetimeIndex(freq='30min', start=merged.index[0], end=merged.index[-1])
-out = pd.DataFrame(index=idx)
-
-out.loc[price.index, 'ImbalancePrice_excess_balance_[£/MWh]'] = price.loc[:, 'Excess balance']
-out.loc[price.index, 'ImbalancePrice_insufficient_balance_[£/MWh]'] = price.loc[:, 'Insufficient balance']
-
-out.loc[vol.index, 'ImbalanceVol_[MW]'] = vol.loc[:, 'imbalanceQuantityMAW']
 
 def print_nans(df):
     nans = df[df.isnull().any(axis=1)]
@@ -51,6 +29,8 @@ def print_nans(df):
     if num != 0:
         print('nan values are:')
         print(nans.head())
+    return num
+
 
 def fill_nans(df):
     print('filling nans in {}'.format(df.columns))
@@ -58,13 +38,56 @@ def fill_nans(df):
     df = df.fillna(method='backfill')
     return df
 
-out = fill_nans(out)
 
 def check_output(df):
     print('final checkout of output')
-    print_duplicates(df)
-    print_nans(df)
+    d = print_duplicates(df)
+    n = print_nans(df)
 
-check_output(out)
+    if d != 0 or n != 0:
+        raise(ValueError('{} duplicates {} nans'.format(d, n)))
 
-out.to_csv('data/clean.csv')
+    print(df.describe())
+    print(' ')
+    print(df.head(1))
+
+
+def clean_price_data():
+    price = pd.read_csv('data/B1770.csv', parse_dates=True)
+
+    price = price.pivot_table(values='imbalancePriceAmountGBP',
+                              index='time_stamp',
+                              columns='priceCategory')
+
+    price.index = pd.to_datetime(price.index)
+
+    return remove_duplicates(price)
+
+
+def clean_vol_data():
+    vol = pd.read_csv('data/B1780.csv', index_col=0, parse_dates=True)
+
+    vol = vol.set_index('time_stamp', drop=True).sort_index()
+
+    vol.index = pd.to_datetime(vol.index)
+
+    return remove_duplicates(vol)
+
+
+if __name__ == '__main__':
+    price = clean_price_data()
+    vol = clean_vol_data()
+
+    merged = pd.concat([price, vol], axis=1)
+
+    idx = pd.DatetimeIndex(freq='30min', start=merged.index[0], end=merged.index[-1])
+    out = pd.DataFrame(index=idx)
+
+    out.loc[price.index, 'ImbalancePrice_excess_balance_[£/MWh]'] = price.loc[:, 'Excess balance']
+    out.loc[price.index, 'ImbalancePrice_insufficient_balance_[£/MWh]'] = price.loc[:, 'Insufficient balance']
+    out.loc[vol.index, 'ImbalanceVol_[MW]'] = vol.loc[:, 'imbalanceQuantityMAW']
+
+    out = fill_nans(out)
+    check_output(out)
+
+    out.to_csv('data/clean.csv')
